2916ef0cdd7adeace0aa77ecb302a41f844742f4
jvillos <jvillosl14@gmail.com>
2024-12-14 19:55:35 +0100

    probando animaciones

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,17 @@
-import "../style/main.scss"; 
+import "../style/main.scss";
+
+// Importar los estilos de AOS
+import 'aos/dist/aos.css';
+
+// Importar la librería JS de AOS
+import AOS from 'aos';
+
+// Inicializar AOS
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        offset: 200,
+        once: false
+    });
+});
